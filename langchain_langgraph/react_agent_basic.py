26df9be443be93d461ebf86d5ff0f65784c78a95
UmarAziz <umar10022004@gmail.com>
2026-04-19 09:11:17 +0500

    feat: strutured output

--- langchain_langgraph/react_agent_basic.py
+++ langchain_langgraph/react_agent_basic.py
@@ -4,9 +4,25 @@ load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
+from typing_extensions import Type,TypedDict,Annotated
+
+# Pydantic models for input and output of tools
+class SearchOutput(BaseModel):
+    """ Search results from Tavily Search tool"""
+    city:str = Field(description="City name")
+    temperature: float = Field(description="Temperature in Celsius")
+    
+    
+# class SearchOutput(TypedDict): # When no validation required, you can also use TypedDict
+#     """ Search results from Tavily Search tool"""
+#     city:Annotated[str, Field(description="City name")]
+#     temperature: Annotated[float, Field(description="Temperature in Celsius")]  
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.9, max_tokens=None)
 search_tool = TavilySearch(max_results=5)
+
+
 @tool
 def get_system_time()-> str:
     """GET current system time"""
@@ -18,7 +34,7 @@ def get_system_time()-> str:
 agent = create_agent(
     model=llm,
     tools=[search_tool, get_system_time],   
-
+    response_format=SearchOutput
 )
 
 result = agent.invoke({
@@ -28,7 +44,6 @@ result = agent.invoke({
 })
 
 print(result["messages"][-1].content)
-print(result)
 # result = llm.invoke("What is the capital of France?")
 
 # print(result)
